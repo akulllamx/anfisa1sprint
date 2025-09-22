7788972c477d0a14405120baebe9f30cfb53f3be
Maxim Akulshin <akulllamx@gmail.com>
2025-09-22 23:25:12 +0400

    22/09/2025 23:25

--- anfisa_for_friends/about/views.py
+++ anfisa_for_friends/about/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 
 def description(request):
     template = 'about/description.html'
+    # template = 'html_templates/about.html'
     return render(request, template)
--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 
 def index(request):
     template = 'homepage/index.html'
+    # template = 'html_templates/index.html'
     return render(request, template)
